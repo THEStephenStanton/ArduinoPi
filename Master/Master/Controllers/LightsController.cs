7ff01d94f81ac59ea8669dbea499edb3f7f51ca4
Stephen Stanton <stephenstanton10@gmail.com>
2018-05-12 00:23:37 -0400

    Created web api for raspberry pi

---
+++ Master/Master/Controllers/LightsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace Master.Controllers
+{
+    [Route("api/[controller]")]
+    public class LightsController : Controller
+    {
+        const string ArduinoIp = "192.168.1.11";
+
+        [HttpGet("{mode}")]
+        public void Get(Power mode)
+        {
+
+            var client = new RestClient($"http://{ArduinoIp}");
+            var request = new RestRequest($"power{mode}", Method.GET);
+
+            client.Execute(request);
+        }
+    }
+}
