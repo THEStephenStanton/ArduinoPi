96953b62143205d7838420d33d29fbcf91dfb084
Stephen Stanton <stephenstanton10@gmail.com>
2018-05-26 16:43:29 -0400

    Added health check

---
+++ Master/Master/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Master.Controllers
+{
+    [Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "pong";
+        }
+    }
+}
